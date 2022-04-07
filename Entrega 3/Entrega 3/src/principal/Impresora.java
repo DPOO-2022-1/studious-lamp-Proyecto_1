b56cf0df135c74a2752af1d7edfb2eb378e0ab79
d.burgos <d.burgos@uniandes.edu.co>
2022-04-06 22:27:38 -0500

    Impresora fun 1

---
+++ Entrega 3/Entrega 3/src/principal/Impresora.java
@@ -0,0 +1,32 @@
+package principal;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+
+public class Impresora {
+
+	public static void CrearArch(String texto, String nombre) {
+
+		try {
+
+			String ruta = "./Data/" + nombre + ".txt";
+
+			File file = new File(ruta);
+
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+			
+			FileWriter fw = new FileWriter(file);
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(texto);
+			bw.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
