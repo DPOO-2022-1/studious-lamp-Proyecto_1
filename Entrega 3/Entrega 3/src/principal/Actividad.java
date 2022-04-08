4c82df6bde1cd21427a35f1593a6a1a32b9f5656
ChusitooXDuwu <88908492+ChusitooXDuwu@users.noreply.github.com>
2022-04-07 22:58:10 -0500

    Logica contador en ACtividad

--- Entrega 3/Entrega 3/src/principal/Actividad.java
+++ Entrega 3/Entrega 3/src/principal/Actividad.java
@@ -1,23 +1,35 @@
 package principal;
 
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime; 
+
 public class Actividad {
 	
 	private String titulo;
 	private String descripcion;
 	private String tipo;
-	private String fecha;
+	private String fecha_inicial;
+	private String fecha_final;
 	private Usuario creador;
-	private String horaTotal;
+	private int horaTotal;
 	private String horaParcial;
 	
-	public Actividad(String title, String description, String type, String date, Usuario creator) 
+	public Actividad(String title, String description, String type, String date_ini, String date_fin,Usuario creator) 
 	{
 		this.setTitulo(title);
 		this.setDescripcion(description);
 		this.tipo= type;
-		this.fecha= date;
+		this.fecha_inicial= date_ini;
+		this.fecha_final= date_fin;
 		this.creador= creator;
 	}
+	
+	// Generador fecha actual
+	public String fecha_actual() {  
+		   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		   LocalDateTime now = LocalDateTime.now();
+		   return (dtf.format(now));
+		  } 
 	
 	// Get y Set
 
@@ -45,12 +57,34 @@ public class Actividad {
 		this.tipo = tipo;
 	}
 
-	public String getFecha() {
-		return fecha;
+	public String getFecha_inicial() {
+		return fecha_inicial;
+	}
+	
+	public String getFecha_final() {
+		return fecha_final;
 	}
 
-	public void setFecha(String fecha) {
-		this.fecha = fecha;
+
+	public void setFecha_inicial(String fecha, Boolean fecha_pasada) {
+		
+		if(fecha_pasada) {
+			this.fecha_inicial = fecha;
+			
+		}else {
+			fecha = fecha_actual();
+			this.fecha_inicial = fecha;
+			}
+	}
+	
+	public void setFecha_final(String fecha, Boolean fecha_pasada) {
+		
+		if(fecha_pasada) {
+			this.fecha_final = fecha;
+			;
+		}else {
+			fecha = fecha_actual();
+			this.fecha_final = fecha;}
 	}
 
 	public Usuario getCreador() {
@@ -61,11 +95,21 @@ public class Actividad {
 		this.creador = creador;
 	}
 
-	public String getHoraTotal() {
+	public int getHoraTotal() {
 		return horaTotal;
 	}
 
-	public void darTiempoTotal(String horaTotal) {
+	public void darTiempoTotal(int horaTotal, Boolean stop_contador) {
+		
+		int tiempo_anios = Integer.parseInt(fecha_final.substring(0,4)) - Integer.parseInt((fecha_inicial.substring(0,4)));
+		int tiempo_meses = Integer.parseInt(fecha_final.substring(5,7)) - Integer.parseInt((fecha_inicial.substring(5,7)));
+		int tiempo_dias = Integer.parseInt(fecha_final.substring(8,10)) - Integer.parseInt((fecha_inicial.substring(8,10)));
+		int tiempo_horas = Integer.parseInt(fecha_final.substring(12,14)) - Integer.parseInt((fecha_inicial.substring(12,14)));
+		
+		horaTotal = (tiempo_anios*8760) + (tiempo_meses*730) + (tiempo_dias*24) + tiempo_horas;
+		
+		
+		
 		this.horaTotal = horaTotal;
 	}
 
